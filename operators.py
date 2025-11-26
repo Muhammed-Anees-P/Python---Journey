6e04ca17bdf4ece0857989217f8372e1a2cf2f2e
muhammed-anees-p <aneespengad4447@gmail.com>
2025-11-26 16:01:35 +0530

    feat(operators): implement arithmetic operators in Python
    - add addition, subtraction, multiplication, and division operators
    - implement modulus, exponentiation, and floor division operators
    - include user input example

---
+++ operators.py
@@ -0,0 +1,77 @@
+#  PYTHON ARITHMETIC OPERATORS
+#  + Addition
+#  - substraction
+#  * multiplication
+#  / division
+# %  modulus
+# ** exponentiation
+# // floor devider
+
+#
+#Example of Addition
+a = 10
+b= 5
+c = a+b
+print(c)
+
+print('==================================================')
+
+
+#Example of Subtraction
+e = 10
+f = 5
+g = e-f
+print(g)
+
+print('==================================================')
+
+
+#Example of Multiplication
+h = 10
+i = 5
+j = h*i
+print(j)
+
+print('==================================================')
+
+
+#Example of Division
+k = 10
+l = 5
+m = k/l
+print(m)
+
+print('==================================================')
+
+
+#Example of Modulus
+n = 10
+o = 3
+p = n%o
+print(p)
+
+print('==================================================')
+
+
+#Example of Exponentiation
+q = 2
+r = 3
+s = q**r
+print(s)
+
+print('==================================================')
+
+
+#Example of Floor devider
+t = 10
+u = 3
+v = t//u
+print(v)
+
+print('==================================================')
+
+
+a = input('enter a number')
+print('number is ' + a)
+ 
+
