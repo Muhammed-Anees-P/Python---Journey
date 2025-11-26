a281a11496380dfcc236a28db1ee05195bf2d6df
muhammed-anees-p <aneespengad4447@gmail.com>
2025-11-26 12:59:13 +0530

    feat(data_types): introduce basic data types in python
    - add examples and explanations for numbers, strings, booleans
    - demonstrate multiple assignment and complex numbers

---
+++ data_types.py
@@ -0,0 +1,75 @@
+# number
+a = 10
+
+print(a)
+print(type(a))
+
+
+print('==================================================')
+
+
+
+#name
+name = "Anees"
+print(name)
+print(type(name))
+
+print('==================================================')
+
+
+# naming variable
+# 1. start with letter or underscore(_)
+# 2. cannot start with number  for example 1name
+# 3. use Alphanumeric only (A-z, a-z, _)
+# 4. case sensitive, eg:  name , Name, NAME,  all variables are different
+
+
+num = 5.28
+x = 10j #save complex number
+print(x)
+print(type(x))
+
+
+print('==================================================')
+
+
+
+#Boolean
+# it true or false
+
+
+python = True
+java = False
+
+print(python)
+print(type(python))
+
+
+print(java)
+print(type(java))
+
+
+print('==================================================')
+
+
+
+
+#
+#Multiple assignment
+h = i = j = k =10
+print(h)
+print(i)
+print(j)
+print(k)
+
+
+print('==================================================')
+
+
+l,m,n = "Car", "Bike", "Bus"
+print(l)
+print(m)
+print(n)
+
+
+
