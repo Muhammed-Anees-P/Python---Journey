57db7f2c9fddea738cb565e316a95ac8f5818ef1
muhammed-anees-p <aneespengad4447@gmail.com>
2025-11-28 10:50:42 +0530

    feat(while_loop): implement while loop examples
    - introduce while loop with condition and increment
    - demonstrate printing numbers while condition is true
    - implement else block execution after loop completion
    - add example of printing increasing asterisks using while loop

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+# while loop 
+# while loop is used to execute a block of statements repeatedly until a given condition is satisfied.
+# And when the condition becomes false, the line immediately after the loop in the program is executed.
+
+i=20
+while i<=10 :
+    print(i)
+    i+=1
+else : 
+    print('End of loop......!')
+
+
+
+rows = 5
+j = 1
+
+while j <= rows :
+    print("*" * j)
+    j+=1
+else :
+    print('executed')    
